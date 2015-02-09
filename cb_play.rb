f958c6c1bdad574ab0d49f73206412acf11ad172
Robby Ranshous <rranshous@gmail.com>
2015-02-09 13:48:47 -0500

    Update cb_play to match simple homework req

--- cb_play.rb
+++ cb_play.rb
@@ -6,20 +6,33 @@ Cb.configure do |config|
   config.time_out   = 5
 end
 
-# available options: keyword, location, postedwithin, 
-# excludeapplyrequirments, orderdirection, orderby,
-# pagenumber, hostsite, siteentity, countrycode,
-# advancedgroupingmode, enablecompanyjobtitlecollapse,
-# groupingvalue
-results = Cb.job.search({ location: 'Atlanta' })
+# ask the important questions
+puts "Where are you looking for a job?"
+where = gets.chomp
+puts "What kind of job are you looking for?"
+what = gets.chomp
+
+# the api wants keywords as comma seperated
+keywords = what.split(" ").join(',')
+
+# Use our CB job helper to search for jobs in atlanta
+results = Cb.job.search({ location: where,
+                          keywords: what })
+
 response = results.response
 response_job_search = response["ResponseJobSearch"]
 errors = response_job_search["Errors"]
 total_count = response_job_search["TotalCount"]
-result_data = response_job_search["Results"]
-real_result_data = result_data["JobSearchResult"]
 
-real_result_data.each do |job_info|
-  puts "TITLE: #{job_info["JobTitle"]}"
-  puts "COMPANY: #{job_info["Company"]}"
+if total_count.to_i == 0
+  puts "NO RESULTS"
+elsif !errors.nil?
+  puts "ERROR!: #{errors.join('|')}"
+else
+  result_data = response_job_search["Results"]
+  real_result_data = result_data["JobSearchResult"]
+
+  real_result_data.each do |job_info|
+    puts "#{job_info["Company"]} :: #{job_info["JobTitle"]}"
+  end
 end
